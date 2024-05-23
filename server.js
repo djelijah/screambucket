f8be9398ca38d589282a3abc1376c1846769c2d7
djelijah <ilijadjordjevic@hotmail.com>
2024-05-23 14:59:38 +0200

    firstcommit

---
+++ server.js
@@ -0,0 +1,84 @@
+const express = require('express');
+const bodyParser = require('body-parser');
+const fs = require('fs');
+const multer = require('multer');
+const path = require('path');
+const { v4: uuidv4 } = require('uuid');
+
+const app = express();
+const PORT = process.env.PORT || 3000;
+
+// Increase the limit for JSON requests (adjust the limit as needed)
+app.use(bodyParser.json({ limit: '10mb' }));
+
+const storage = multer.diskStorage({
+    destination: (req, file, cb) => {
+        cb(null, 'uploads/');
+    },
+    filename: (req, file, cb) => {
+        cb(null, `${uuidv4()}.wav`); // Save the file with a .wav extension
+    }
+});
+
+const upload = multer({ storage: storage });
+
+app.use(bodyParser.json());
+app.use(express.static('public'));
+
+// Utility function to read and parse JSON file safely
+function readJSONFileSync(filePath) {
+    if (fs.existsSync(filePath)) {
+        const fileContent = fs.readFileSync(filePath, 'utf-8');
+        if (fileContent) {
+            return JSON.parse(fileContent);
+        }
+    }
+    return [];
+}
+
+// Endpoint to save drawing data
+app.post('/save-drawing', (req, res) => {
+    const newLine = req.body.lines;
+    let drawingData = readJSONFileSync('drawingData.json');
+
+    drawingData = newLine; // replace the whole drawing data with the new data
+    fs.writeFileSync('drawingData.json', JSON.stringify(drawingData, null, 2));
+    res.sendStatus(200);
+});
+
+// Endpoint to retrieve drawing data
+app.get('/get-drawing', (req, res) => {
+    const drawingData = readJSONFileSync('drawingData.json');
+    res.json(drawingData);
+});
+
+// Endpoint to save audio file
+app.post('/upload-audio', upload.single('audio'), (req, res) => {
+    const audioFile = req.file;
+    const audioData = {
+        id: uuidv4(),
+        filename: audioFile.filename,
+        originalname: audioFile.originalname,
+        mimetype: audioFile.mimetype,
+        size: audioFile.size,
+    };
+
+    let audioFiles = readJSONFileSync('audioData.json');
+    audioFiles.push(audioData);
+    fs.writeFileSync('audioData.json', JSON.stringify(audioFiles, null, 2));
+    res.sendStatus(200);
+});
+
+// Endpoint to retrieve audio files
+app.get('/get-audio', (req, res) => {
+    const audioFiles = readJSONFileSync('audioData.json');
+    res.json(audioFiles);
+});
+
+// Serve the uploaded audio files
+app.use('/uploads', express.static(path.join(__dirname, 'uploads')));
+
+// Start the server
+app.listen(PORT, () => {
+    console.log(`Server is running on port ${PORT}`);
+});
